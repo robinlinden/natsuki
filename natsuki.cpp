2c222e92e9753e70acc99b49652ffa9a350a8dec
Robin Linden <dev@robinlinden.eu>
2021-05-16 01:28:58 +0200

    Add basic subscription support

--- natsuki.cpp
+++ natsuki.cpp
@@ -8,6 +8,23 @@
 int main(int argc, char **argv) try {
     natsuki::Nats nats{argc >= 2 ? argv[1] : "localhost"};
     std::thread nats_thread{&natsuki::Nats::run, &nats};
+
+    nats.subscribe("natsuki.running", [](std::string_view data) {
+        std::cout << "running: " << data << "\n";
+    });
+
+    nats.subscribe("natsuki.not_running", [](std::string_view data) {
+        std::cout << "not_running: " << data << "\n";
+    });
+
+    nats.subscribe("natsuki.ping", [](std::string_view data) {
+        std::cout << "ping 1: " << data << "\n";
+    });
+
+    nats.subscribe("natsuki.ping", [](std::string_view data) {
+        std::cout << "ping 2: " << data << "\n";
+    });
+
     nats.publish("natsuki.running", "true");
     nats.publish("natsuki.ping", "", "natsuki.pong");
     std::this_thread::sleep_for(std::chrono::seconds(5));
--- natsuki.h
+++ natsuki.h
@@ -3,10 +3,13 @@
 
 #include <asio.hpp>
 
+#include <atomic>
+#include <charconv>
 #include <cstring>
 #include <functional>
 #include <iostream>
 #include <istream>
+#include <map>
 #include <optional>
 #include <stdexcept>
 #include <string>
@@ -54,6 +57,17 @@ public:
         });
     }
 
+    void subscribe(std::string_view subject, std::function<void(std::string_view)> cb) {
+        std::stringstream ss;
+        int sid = next_subscription_id_.fetch_add(1);
+        ss << "SUB " << subject << " " << sid << "\r\n";
+
+        asio::post(io_context_, [this, sid, cb, msg = ss.str()] {
+            subscriptions_[sid] = cb;
+            asio::write(socket_, asio::buffer(msg));
+        });
+    }
+
 private:
     void on_read(asio::error_code const &ec, std::size_t bytes_transferred) {
         using namespace std::literals;
@@ -69,7 +83,21 @@ private:
         buf_.consume(bytes_transferred);
         std::cout << data << std::endl;
 
-        if (data.starts_with("INFO")) {
+        if (data.starts_with("MSG")) {
+            // Parse out the subscription id.
+            std::size_t sid_location = data.find_first_of(" ", strlen("MSG ")) + 1;
+            int sid = -1;
+            std::from_chars(data.data() + sid_location, data.data() + data.size(), sid);
+
+            // Get the payload data.
+            std::size_t payload_length = asio::read_until(socket_, buf_, "\r\n");
+            std::string payload{
+                asio::buffers_begin(buf_.data()),
+                asio::buffers_begin(buf_.data()) + payload_length - std::strlen("\r\n")};
+            buf_.consume(payload_length);
+
+            subscriptions_.at(sid)(payload);
+        } else if (data.starts_with("INFO")) {
             auto connect{"CONNECT {\"verbose\":true,\"pedantic\":true,\"name\":\"natsuki\",\"lang\":\"cpp\",\"version\":\"0.0.1\",\"protocol\":0}\r\n"sv};
             asio::write(socket_, asio::buffer(connect));
         } else if (data.starts_with("PING")) {
@@ -88,6 +116,8 @@ private:
     asio::ip::tcp::socket socket_{io_context_};
     asio::streambuf buf_{};
     std::string address_{};
+    std::atomic<int> next_subscription_id_{};
+    std::map<int, std::function<void(std::string_view)>> subscriptions_{};
 };
 
 } // namespace natsuki
