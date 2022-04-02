bc70266f4a9d3e384ae40391ca74be4a33aaddbd
Robin Linden <dev@robinlinden.eu>
2022-04-02 14:12:59 +0200

    Improve payload handling in bench

--- example/bench.cc
+++ example/bench.cc
@@ -4,8 +4,11 @@
 
 #include "natsuki/natsuki.h"
 
+#include <algorithm>
 #include <chrono>
+#include <ctime>
 #include <iostream>
+#include <random>
 #include <stdexcept>
 #include <string>
 #include <string_view>
@@ -14,9 +17,29 @@
 
 using namespace std::literals;
 
+namespace {
+
+std::string random_payload(int length, unsigned seed) {
+    static constexpr auto chars =
+            "0123456789"
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "abcdefghijklmnopqrstuvwxyz"
+            "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+            "\"\\";
+    auto gen = std::mt19937{};
+    gen.seed(seed);
+    auto dist = std::uniform_int_distribution{std::size_t{0}, std::strlen(chars) - 1};
+    auto result = std::string(length, '\0');
+    std::generate_n(begin(result), length, [&] { return chars[dist(gen)]; });
+    return result;
+}
+
+} // namespace
+
 int main(int argc, char **argv) try {
     auto address = "localhost"s;
     int msgs = 1'000'000;
+    int payload_size = 10;
 
     for (int i = 1; i < argc; ++i) {
         if (argv[i] == "--msgs"sv) {
@@ -36,13 +59,19 @@ int main(int argc, char **argv) try {
         }
     }
 
-    std::cout << "Benchmarking against NATS server at " << address << ".\n";
+    auto seed = static_cast<unsigned>(std::time(nullptr));
+
+    std::cout << "Benchmarking with seed " << seed
+            << " and payload size " << payload_size
+            << " against NATS server at " << address << ".\n";
+
+    auto payload = random_payload(payload_size, seed);
     natsuki::Nats nats{std::move(address)};
     std::thread nats_thread{&natsuki::Nats::run, &nats};
 
     auto const start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < msgs; ++i) {
-        nats.publish("bench"sv, "data"sv);
+        nats.publish("bench"sv, payload);
     }
     auto const end = std::chrono::high_resolution_clock::now();
     std::cout << "Published " << msgs << " messages in "
