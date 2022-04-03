2e7ce84160f78a45b67714c14359997c9c5283fb
Robin Linden <dev@robinlinden.eu>
2022-04-04 00:42:27 +0200

    Support using more than one publisher in bench

--- example/bench.cc
+++ example/bench.cc
@@ -9,12 +9,14 @@
 #include <ctime>
 #include <iomanip>
 #include <iostream>
+#include <list>
 #include <random>
 #include <stdexcept>
 #include <string>
 #include <string_view>
 #include <thread>
 #include <utility>
+#include <vector>
 
 using namespace std::literals;
 
@@ -42,6 +44,7 @@ int main(int argc, char **argv) try {
     int msgs = 1'000'000;
     int payload_size = 10;
     auto seed = static_cast<unsigned>(std::time(nullptr));
+    int publisher_count = 1;
 
     for (int i = 1; i < argc; ++i) {
         if (argv[i] == "--msgs"sv) {
@@ -77,6 +80,17 @@ int main(int argc, char **argv) try {
             continue;
         }
 
+        if (argv[i] == "--pub"sv) {
+            if (i + 1 == argc) {
+                std::cout << "Missing count after --pub\n";
+                return 1;
+            }
+
+            publisher_count = std::stoi(argv[i + 1]);
+            ++i;
+            continue;
+        }
+
         // Positional.
         if (i == argc - 1) {
             address = argv[i];
@@ -88,22 +102,42 @@ int main(int argc, char **argv) try {
             << " against NATS server at " << address << ".\n";
 
     auto payload = random_payload(payload_size, seed);
-    natsuki::Nats nats{std::move(address)};
-    std::thread nats_thread{&natsuki::Nats::run, &nats};
+
+    std::list<natsuki::Nats> publishers;
+    std::list<std::thread> threads;
+    for (int i = 0; i < publisher_count; ++i) {
+        auto &nats = publishers.emplace_back(address);
+        threads.emplace_back(&natsuki::Nats::run, &nats);
+    }
 
     auto const start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < msgs; ++i) {
-        nats.publish("bench"sv, payload);
+    std::vector<std::thread> runners;
+    runners.reserve(publisher_count);
+    for (auto &nats : publishers) {
+        runners.emplace_back([&] {
+            for (int j = 0; j < msgs / publisher_count; ++j) {
+                nats.publish("bench"sv, payload);
+            }
+        });
     }
+
+    for (int i = 0; i < publisher_count; ++i) {
+        runners[i].join();
+    }
+
     auto const end = std::chrono::high_resolution_clock::now();
+
+    for (auto &nats : publishers) {
+        nats.shutdown();
+        threads.front().join();
+        threads.pop_front();
+    }
+
     auto const duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
     auto const msgs_per_second = static_cast<float>(msgs) / duration.count() * 1000; // msgs/ms -> msgs/s
     std::cout << std::fixed << std::setprecision(0)
             << "Published " << msgs << " messages in "
             << duration.count() << "ms. (" << msgs_per_second << " msgs/s)\n";
-
-    nats.shutdown();
-    nats_thread.join();
 } catch (std::exception const &e) {
     std::cerr << e.what();
     throw;
