02a3abe6fa30a5f3e3d1d9137443837c5a278f9d
Robin Linden <dev@robinlinden.eu>
2022-05-06 18:33:05 +0200

    bench: Support printing the result as JSON

--- bench/main.cc
+++ bench/main.cc
@@ -4,6 +4,7 @@
 
 #include "bench/arg_parser.h"
 #include "bench/bench.h"
+#include "bench/json_reporter.h"
 #ifdef NATSC_BENCH
 #include "bench/natsc_publisher.h"
 #include "bench/natsc_subscriber.h"
@@ -16,10 +17,12 @@
 
 #include <exception>
 #include <iostream>
+#include <memory>
 #include <utility>
 
 int main(int argc, char **argv) try {
     bench::Options opts;
+    bool want_json = false;
 
     bench::ArgParser()
             .argument("--msgs", opts.messages)
@@ -27,10 +30,17 @@ int main(int argc, char **argv) try {
             .argument("--seed", opts.seed)
             .argument("--pub", opts.publisher_count)
             .argument("--sub", opts.subscriber_count)
+            .argument("--json", want_json)
             .positional(opts.address)
             .parse(argc, argv);
 
-    bench::PlainTextReporter listener{};
+    auto listener = [&]() -> std::unique_ptr<bench::IBenchmarkListener> {
+        if (want_json) {
+            return std::make_unique<bench::JsonReporter>();
+        }
+        return std::make_unique<bench::PlainTextReporter>();
+    }();
+
 #ifdef NATSC_BENCH
     bench::NatscPublisherFactory pub_factory{};
     bench::NatscSubscriberFactory sub_factory{};
@@ -38,7 +48,7 @@ int main(int argc, char **argv) try {
     bench::NatsukiPublisherFactory pub_factory{};
     bench::NatsukiSubscriberFactory sub_factory{};
 #endif
-    bench::run_bench(listener, std::move(opts), pub_factory, sub_factory);
+    bench::run_bench(*listener, std::move(opts), pub_factory, sub_factory);
 } catch (std::exception const &e) {
     std::cerr << e.what();
     throw;
